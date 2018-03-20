4223ce9253a216738b99882eeba0c061e362358b
Tuulikki Laine <tuulikki.laine2@gmail.com>
2018-03-20 12:11:22 +0200

    remove function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-/**app**/
-
 function Ao(props) {
     return (
         <div className="ao_wrapper">
             <div>{props.name}</div>
-            <button onClick={() => this.remove(props.id)}>X</button>
+            <button onClick={props.onClick}>X</button>
         </div>
     );
 }
@@ -15,7 +13,7 @@ class App extends React.Component {
         this.state = {
             raw_data: null,
             components: null,
-            ao: []
+            ao_list: []
         };
     }
     componentDidMount () {
@@ -23,30 +21,34 @@ class App extends React.Component {
         .then(response => response.json())
         .then(data => this.setState({ raw_data: data, components:JSON.parse(JSON.stringify(data)) }));
     }
-    remove(id) {
-        
+    remove(list,obj,data) {
+        let new_ao_list = list.filter(item => item.id != obj.id);
+        this.setState({
+            ao_list:new_ao_list
+        })
+        data.push(obj);
     }
     random_ao(array) {
         if (array.length == 0) {
             this.setState({
                 components: JSON.parse(JSON.stringify(this.state.raw_data)),
-                ao: []
+                ao_list: []
             })
             return;
         }
         let rand = Math.floor(Math.random()*array.length);
         let chosen = array[rand];
         array.splice(rand,1);
-        let new_ao = this.state.ao.concat([chosen])
+        let new_ao_list = this.state.ao_list.concat([chosen])
         this.setState({
-            ao:new_ao
+            ao_list:new_ao_list
         })
     }
     render() {
         return(
             <div className="randomizer">
             <button onClick={() => this.random_ao(this.state.components.ancient_ones)}>Randomize</button>
-            {this.state.ao.map((obj) => <Ao name={obj.name} />)}
+            {this.state.ao_list.map((obj) => <Ao name={obj.name} onClick={() => this.remove(this.state.ao_list,obj,this.state.components.ancient_ones)}/>)}
             </div>
             
         )
